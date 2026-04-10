d40d03e357b0b6570560b10be165afbdc20886c2
“iker777” <ikeralvarez777@gmail.com>
2026-04-10 10:50:17 +0200

    First commit fixed

--- app/page.jsx
+++ app/page.jsx
@@ -1,17 +1,12 @@
-// import { getPeliculasPopulares } from "@/lib/apiSearch";
-
 import ContenedorPelis from "@/components/ContenedorPelis";
 
-
 export default async function Home() {
-  // const peliculas = await getPeliculasPopulares()
+
   return (
     <>
       <h1 className="py-6 uppercase">Películas destacadas</h1>
       <ContenedorPelis />
-      {/* {
-        peliculas.map(p => <img src={`https://image.tmdb.org/t/p/w500/${p.backdrop_path}`} />)
-      } */}
+     
     </>
   );
 }
