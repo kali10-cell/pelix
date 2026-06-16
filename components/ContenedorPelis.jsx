6bb5581e68893985c10a0f94cc26b25adc5c204b
Raven <cabrerasusy68@gmail.com>
2026-06-16 10:36:35 +0200

    Usa Supabase para ordenar pelis

--- components/ContenedorPelis.jsx
+++ components/ContenedorPelis.jsx
@@ -1,14 +1,19 @@
-import { fetchPeliculasPopulares } from "@/lib/tmdbApi";
+import { fetchPeliculasPorIds, fetchPeliculasPopulares } from "@/lib/tmdbApi";
+import { fetchPelisOrdenadas } from "@/lib/supabasePelis";
 import TarjetaPeli from "./TarjetaPeli";
 
 
 
 export default async function ContenedorPelis() {
-    const { results } = await fetchPeliculasPopulares() // Array
+    const idsOrdenados = await fetchPelisOrdenadas();
+    const { results } = idsOrdenados.length
+        ? await fetchPeliculasPorIds(idsOrdenados)
+        : await fetchPeliculasPopulares(); // Array
+
     return (
         <section className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 w-full px-2">
             {
-                results.map(p => <TarjetaPeli key={p.id} peliData={p} />)
+                results?.map(p => <TarjetaPeli key={p.id} peliData={p} />)
             }
         </section>
     )
